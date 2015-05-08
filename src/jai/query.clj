794dad614942a0005fd1cce1b2ac7e5a5a719c77
Chris Zheng <z@caudate.me>
2015-05-08 12:10:37 +0530

    now queryable and with traversal

--- src/jai/query.clj
+++ src/jai/query.clj
@@ -1,5 +1,9 @@
 (ns jai.query
-  (:require [jai.common :as common]))
+  (:require [jai.common :as common]
+            [jai.match :as match]
+            [jai.query.traverse :as traverse]
+            [jai.query.walk :as query]
+            [clojure.walk :as walk]))
 
 (defn cursor-info [selectors]
   (let [candidates
@@ -12,16 +16,16 @@
                                  ele)) [i :form ele]))))]
     (case (count candidates)
       0 (if (list? (last selectors))
-          [(dec (count selectors)) :form]
+          [(dec (count selectors)) :form (last selectors)]
           [nil :cursor])
       1 (let [max      (dec (count selectors))
-              [i type] (first candidates)
+              [i type :as candidate] (first candidates)
               _ (case type
                   :form   (if (not= i max)
                             (throw (Exception. "Form should be in the last position of the selectors")))
                   :cursor (if (= i max)
                             (throw (Exception. "Cursor cannot be in the last position of the selectors"))))]
-          [i type])
+          candidate)
       (throw (ex-info (format "There should only be one of %s in the path." ) 
                       {:candidates candidates})))))
 
@@ -46,22 +50,119 @@
                         :else (throw (Exception. "Should not be here")))]
     {:up up :down down}))
 
-(defn jai [context selectors]
+
+(defn process-special [ele]
+  (if (keyword? ele)
+    (or (if (= :* ele) {:type :multi})
+        (if-let [step (try (Integer/parseInt (name ele))
+                           (catch java.lang.NumberFormatException e))]
+          (if (> step 0)
+            {:type :nth :step step}))
+        (throw (ex-info "Not a valid keyword (either :* or :<natural number>)" {:value ele})))))
+
+(defn process-path
+  ([path] (process-path path []))
+  ([[x y & xs :as more] out]
+   (if-not (empty? more)
+     (let [xmap  (process-special x)
+           xmeta (meta x)
+           ymap  (process-special y)
+           ymeta (meta y)]
+       (cond (and (nil? xmap) (= 1 (count more)))
+             (conj out (merge {:type :step :element x} xmeta))
+             
+             (nil? xmap)
+             (recur (cons y xs)
+                    (conj out (merge {:type :step :element x} xmeta)))
+             
+             (and xmap ymap)    
+             (recur (cons y xs)
+                    (conj out (assoc xmap :element '_)))
+             
+             (and xmap (= 1 (count more)))
+             (conj out (assoc xmap :element '_))
+             
+             :else
+             (recur xs
+                    (conj out (merge (assoc xmap :element y) ymeta)))))
+     out)))
+
+(defn compile-section-base [section]
+  (let [{:keys [element] evaluate? :%} section]
+    (cond evaluate?
+          (compile-section-base (-> section
+                                    (assoc :element (eval element))
+                                    (dissoc :%)))
+
+          (= '_ element)    {:is common/any}
+          (fn? element)     {:is element}
+          (map? element)    (walk/postwalk
+                             (fn [ele]
+                               (cond (:% (meta ele))
+                                     (eval (with-meta ele
+                                             (-> (meta ele)
+                                                 (dissoc :%))))
+                                     :else ele))
+                             element)
+          (list? element)   {:pattern element}
+          (symbol? element) {:form element}
+          :else {:is element})))
+
+(def moves
+  {:step   {:up     :parent
+            :down   :child}
+   :multi  {:up     :ancestor
+            :down   :contains}
+   :nth    {:up     :nth-ancestor
+            :down   :nth-contains}})
+
+(defn compile-section [direction prev {:keys [type step] optional? :? :as section}]
+  (let [base (-> section
+                 compile-section-base)
+        dkey (get-in moves [type direction])
+        current (merge base prev)
+        current (if optional?
+                  {:or #{current (merge {:is common/any} prev)}}
+                  current)]
+    (if (= type :nth)
+      {dkey [step current]}
+      {dkey current})))
+
+(defn compile-submap [direction sections]
+  (reduce (fn [i section]
+            (compile-section direction i section))
+          nil sections))
+
+(defn select [zloc selectors]
   (let [selectors  (expand-all-metas selectors)
-        cursor     (cursor-info selectors)
+        [cidx ctype cform :as cursor]     (cursor-info selectors)
         qselectors (mapv (fn [ele]
                            (if (list? ele)
                              (common/prepare-query ele) ele))
                          selectors)
-        path-map   (split-path qselectors cursor)]
-    [cursor path-map]))
+        {:keys [up down]}   (split-path qselectors cursor)
+        up (process-path up)
+        [curr & down] (process-path down)
+        match-map (merge (compile-section-base curr)
+                         (compile-submap :up up)
+                         (compile-submap :down down))
+        match-fn (match/compile-matcher match-map)]
+      (let [atm  (atom [])]
+        (query/matchwalk zloc
+                         [match-fn]
+                         (fn [zloc]
+                           (if (= :form ctype)
+                             (swap! atm conj (traverse/traverse zloc cform))
+                             (swap! atm conj zloc))
+                           zloc))
+    @atm)))
 
 (defmacro $ [context selectors]
-  `(jai ~context (quote ~selectors)))
-
+  `(select ~context (quote ~selectors)))
 
 (comment
   (jai nil '[d :* (defn ^:?&- _ | & _)])
+  (jai nil '[d :* | (defn & _)])
 
 
   (potential-cursors '[(defn ^:?& _ | & _)])
--- test/jai/query/traverse_test.clj
+++ test/jai/query/traverse_test.clj
@@ -1,1 +1,63 @@
-(ns jai.query.traverse-test)
+(ns jai.query.traverse-test
+  (:use midje.sweet)
+  (:require [rewrite-clj.zip :as source]
+            [clojure.zip :as zip]
+            [jai.query.traverse :refer :all]))
+
+(fact
+  (source/sexpr
+   (traverse (source/of-string "^:a (+ () 2 3)")
+             '(+ () 2 3)))
+  => '(+ () 2 3)
+  
+  (source/sexpr
+   (traverse (source/of-string "^:a (hello)")
+             '(hello)))
+  => '(hello)
+  
+  (source/sexpr
+   (traverse (source/of-string "^:a (hello)")
+             '(^:- hello)))
+  => ()
+  
+  (source/sexpr
+   (traverse (source/of-string "(hello)")
+             '(^:- hello)))
+  => ()
+  
+  (source/sexpr
+   (traverse (source/of-string "((hello))")
+             '((^:- hello))))
+  => '(())
+
+  ;; Insertions
+  (source/sexpr
+   (traverse (source/of-string "()")
+             '(^:+ hello)))
+  => '(hello)
+  
+  (source/sexpr
+   (traverse (source/of-string "(())")
+             '((^:+ hello))))
+  => '((hello)))
+
+(fact
+  (source/sexpr
+   (traverse (source/of-string "(defn hello)")
+             '(defn ^{:? true :% true} symbol? ^:+ [])))
+  => '(defn hello [])
+  
+  (source/sexpr
+   (traverse (source/of-string "(defn hello)")
+             '(defn ^{:? true :% true :- true} symbol? ^:+ [])))
+  => '(defn [])
+  
+  (source/sexpr
+   (traverse (source/of-string "(defn hello)")
+             '(defn ^{:? true :% true :- true} symbol? | ^:+ [])))
+  => []
+  
+  (source/sexpr
+   (traverse (source/of-string "(defn hello [] (+ 1 1))")
+             '(defn _ _ (+ | 1 & _))))
+  => 1)
